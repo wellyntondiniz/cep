97302e8beb9eeb651c107e06be4c7523c9a3af48
Wellynton Diniz <wellyntondiniz@gmail.com>
2024-10-08 20:00:53 -0400

    Endpoints

--- src/main/java/com/ifmt/cep/CepRestController.java
+++ src/main/java/com/ifmt/cep/CepRestController.java
@@ -3,7 +3,12 @@ package com.ifmt.cep;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +22,21 @@ public class CepRestController {
 	@GetMapping
 	public List<Cep> buscarTodos() {
 		return cepService.buscarTodos();
+	}
+	
+	@PostMapping
+	public void inserir(@RequestBody Cep cep) {
+		cepService.inserir(cep);
+	}
+	
+	@DeleteMapping(value = "{id}")
+	public void remover(@PathVariable Integer id) {
+		cepService.remover(id);
+	}
+	
+	@PutMapping
+	public Cep alterar(@RequestBody Cep cep) {
+		return cepService.alterar(cep);
 	}
 	
 }
--- src/main/java/com/ifmt/cep/CepService.java
+++ src/main/java/com/ifmt/cep/CepService.java
@@ -8,11 +8,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class CepService {
 
-	@Autowired
 	CepRepository cepRepository;
-	
+
+	public CepService(CepRepository cepRepository) {
+		super();
+		this.cepRepository = cepRepository;
+	}
+
 	public List<Cep> buscarTodos() {
 		return cepRepository.findAll();
+	}
+	
+	public Cep inserir(Cep cep) {
+		return cepRepository.save(cep);
+	}
+	
+	public void remover(Integer id) {
+		Cep cep = cepRepository.findById(id).get();
+		cepRepository.delete(cep);
+	}
+	
+	public Cep alterar (Cep cep) {
+		return cepRepository.save(cep);
 	}
 	
 }
